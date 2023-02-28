f6924e9406ccd20309495c88e1e75dae2b49af2f
Andre <andredelima92@gmail.com>
2023-02-27 21:33:33 -0300

    test

---
+++ prisma/migrations/20230113012723_test/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `price` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `Product` ADD COLUMN `price` INTEGER NOT NULL,
+    ADD COLUMN `userId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
